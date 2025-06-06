1cb7212c42393eca1b677877b1eea00239e48968
zaracky <51820857+zaracky@users.noreply.github.com>
2025-06-06 16:27:25 +0200

    Finalisation

--- Openagenda.py
+++ Openagenda.py
@@ -4,6 +4,7 @@ import time
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from datetime import datetime
 from langchain_core.documents import Document
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_mistralai import MistralAIEmbeddings
@@ -13,12 +14,10 @@ from tqdm import tqdm
 load_dotenv()
 api_key = os.getenv('MISTRAL_AI_KEY')
 
-# Initialisation des embeddings Mistral AI
+# — Initialisation des embeddings Mistral AI
 embeddings = MistralAIEmbeddings(model="mistral-embed", api_key=api_key)
 
-
 def nettoyer_texte(texte):
-    """Nettoyer un texte (HTML, caractères spéciaux, normalisation)."""
     texte = BeautifulSoup(texte, "html.parser").get_text()
     texte = texte.lower()
     texte = re.sub(r'[^\w\s.,!?;:\'\"À-ÿ]', ' ', texte)
@@ -26,8 +25,7 @@ def nettoyer_texte(texte):
     return texte
 
 def obtenir_evenements_structures():
-    """Récupère les événements culturels filtrés et formatés depuis l'API OpenAgenda."""
-    start_year = 2025
+    start_year = datetime.now().year
     location = "Occitanie"
     results = []
     event_types = ["cinema", "festival", "concert", "danse", "spectacle", "théâtre", "jazz", "exposition",
@@ -40,7 +38,7 @@ def obtenir_evenements_structures():
         total_count = response.json().get("total_count", 0)
 
         for offset_index in range(int(total_count / 100) + 1):
-            offset_url = f"https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/evenements-publics-openagenda/records?limit=100&offset={offset_index * 100}&refine=keywords_fr%3A%22{event_type}%22&refine=firstdate_begin%3A%222024%22&refine=location_region%3A%22{location}%22"
+            offset_url = f"https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/evenements-publics-openagenda/records?limit=100&offset={offset_index * 100}&refine=keywords_fr%3A%22{event_type}%22&refine=firstdate_begin%3A%22{start_year}%22&refine=location_region%3A%22{location}%22"
             offset_response = requests.get(offset_url)
             results += offset_response.json().get("results", [])
             time.sleep(0.5)
@@ -65,7 +63,6 @@ def obtenir_evenements_structures():
     return df
 
 def generer_documents(df):
-    """Convertit un DataFrame en objets `Document` pour Langchain."""
     documents = []
     for _, row in df.iterrows():
         content = row["content"]
@@ -101,23 +98,9 @@ def generer_documents(df):
     return documents
 
 def decouper_documents(documents):
-    """
-    Découpe les documents en morceaux plus petits, en affichant une barre de progression.
-
-    Args:
-        documents (List[Document]): Liste des documents à découper
-
-    Returns:
-        List[Document]: Liste des documents découpés
-    """
-    # Initialiser la barre de progression
-    print("✂️ Découpage sémantique des documents...")
+    print("\u2702\ufe0f Découpage sémantique des documents...")
     text_splitter = SemanticChunker(embeddings)
-
-    # Utilisation de tqdm pour la barre de progression
     splitted_docs = []
     for doc in tqdm(documents, desc="Découpe des documents", unit="document"):
-        splitted_docs.extend(text_splitter.create_documents([doc.page_content]))  # Utilisation du texte brut
-
+        splitted_docs.extend(text_splitter.create_documents([doc.page_content]))
     return splitted_docs
-
